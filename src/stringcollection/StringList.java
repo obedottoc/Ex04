863f9cda302c4c82a356da18082029b934280b66
user3 <user3@simlab14>
2019-08-16 11:29:26 +0530

    stringcollection

---
+++ src/stringcollection/StringList.java
@@ -0,0 +1,83 @@
+/***
+ * This program is maintain the string of numbers.
+ *Developed by A.sandhiya.
+ *student,Saveetha Engineering College.
+ */
+package stringcollection;
+
+import java.util.*;
+
+public class StringList {
+	public static void main(String[] args) {
+		int option;
+		String n;
+		int index,i;
+		Scanner sc=new Scanner(System.in);
+		ArrayList<String>str;
+		
+		
+		str=new ArrayList<String>();
+		
+		while(true)
+		{
+			System.out.println("1. To add an string at the end");
+			System.out.println("2. To insert an string at particular index");
+			System.out.println("3. To search for a string");
+			System.out.println("4. To display all the strings");
+			System.out.println("5. Exit");
+			System.out.print("Enter your choice:");
+			
+			option=sc.nextInt();
+			switch(option)
+			{
+			case 1:
+				System.out.print("Enter a string:");
+				n=sc.next();
+				str.add(n);
+				System.out.println("The given string is added at the end");
+				break;
+			case 2:
+				System.out.print("Enter a string:");
+				n=sc.next();
+				System.out.print("Enter the index:");
+				index=sc.nextInt();				
+				str.add(index,n);
+				System.out.println("The given number is added at the given index");				
+				break;
+			case 3:
+				System.out.print("Enter a number:");
+				n=sc.next();
+				index=str.indexOf(n);
+				if(index<0)
+				{
+					System.out.println("The given number is not available in the list");
+				}else
+				{
+					System.out.printf("The number %d is found at the index %d\n",n,index);
+				}
+				break;
+			case 4:
+				System.out.println("The available numbers are:");
+				for(i=0;i<str.size();i++)
+				{
+					System.out.println(str.get(i));
+				}
+				break;
+			case 5:
+				System.out.println("Thankyou for using number list application !!!");
+				break;
+			default:
+				System.out.println("Please enter a valid number !!!");
+			}
+			
+			if(option==5)
+			{		
+				break;
+			}
+		}
+
+	}
+
+}
+
+
