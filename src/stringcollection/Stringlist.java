2d574a93ada687079e4e243cc309034931bf2368
user3 <user3@administrator-HP-Pro-3330-MT>
2019-09-16 08:27:37 +0530

    To create a string list

---
+++ src/stringcollection/Stringlist.java
@@ -0,0 +1,101 @@
+/*
+* This program is maintain the list of numbers
+* Developed by monica
+* Saveetha Engineering College
+* monicarajavel26@gmail.com
+*
+*/
+package stringcollection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
+public class Stringlist {
+
+public static void main(String[] args) {
+
+int option,index,i;
+String n,s;
+Scanner sc=new Scanner(System.in);
+ArrayList alpha;
+
+
+alpha=new ArrayList();
+
+while(true)
+{
+System.out.println("1. To add an string at the end");
+System.out.println("2. To insert an string at particular index");
+System.out.println("3. To search for a string");
+System.out.println("4. To display all the string");
+System.out.println("5. To List all string starts with given letter");
+
+System.out.println("6. Exit");
+System.out.print("Enter your choice:");
+
+option=sc.nextInt();
+switch(option)
+{
+case 1:
+System.out.print("Enter a string:");
+n=sc.next();
+alpha.add(n);
+System.out.println("The given string is added at the end");
+break;
+case 2:
+System.out.print("Enter a string:");
+n=sc.next();
+System.out.print("Enter the index:");
+index=sc.nextInt();
+alpha.add(index,n);
+System.out.println("The given string is added at the given index");
+break;
+case 3:
+System.out.print("Enter a string:");
+n=sc.next();
+index=alpha.indexOf(n);
+if(index<0)
+{
+System.out.println("The given string is not available in the list");
+}else
+{
+System.out.printf("The string %s is found at the index %s\n",n,index);
+}
+break;
+case 4:
+System.out.println("The available string are:");
+for(i=0;i<alpha.size();i++) {
+	System.out.println(alpha.get(i));
+}
+break;
+case 5:
+
+
+{
+System.out.println("enter string starting letter");
+s=sc.next();
+for(i=0;i<alpha.size();i++) {
+if (((String) alpha.get(i)).startsWith(s)){
+System.out.println(alpha.get(i));
+}
+}
+break;
+}
+
+
+case 6:
+System.out.println("Thankyou for using string list application !!!");
+break;
+default:
+System.out.println("Please enter a valid number !!!");
+}
+
+if(option==6)
+{
+break;
+}
+}
+
+}
+}
