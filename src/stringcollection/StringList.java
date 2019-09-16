a8a7fe6026c99a633a86ad3500ada2fb52ff6850
user3 <user3@simlab14>
2019-09-16 09:08:57 +0530

    stringcollection

--- src/stringcollection/StringList.java
+++ src/stringcollection/StringList.java
@@ -24,7 +24,8 @@ public class StringList {
 			System.out.println("2. To insert an string at particular index");
 			System.out.println("3. To search for a string");
 			System.out.println("4. To display all the strings");
-			System.out.println("5. Exit");
+			System.out.println("5. To list  all strings starts with given letter");
+			System.out.println("6. Exit");
 			System.out.print("Enter your choice:");
 			
 			option=sc.nextInt();
@@ -42,42 +43,53 @@ public class StringList {
 				System.out.print("Enter the index:");
 				index=sc.nextInt();				
 				str.add(index,n);
-				System.out.println("The given number is added at the given index");				
+				System.out.println("The given String is added at the given index");				
 				break;
 			case 3:
-				System.out.print("Enter a number:");
+				System.out.print("Enter a String:");
 				n=sc.next();
 				index=str.indexOf(n);
 				if(index<0)
 				{
-					System.out.println("The given number is not available in the list");
+					System.out.println("The given String is not available in the list");
 				}else
 				{
-					System.out.printf("The number %d is found at the index %d\n",n,index);
+					System.out.printf("The String %d is found at the index %d\n",n,index);
 				}
 				break;
 			case 4:
-				System.out.println("The available numbers are:");
+				System.out.println("The available strings are:");
 				for(i=0;i<str.size();i++)
+				
 				{
 					System.out.println(str.get(i));
 				}
 				break;
 			case 5:
-				System.out.println("Thankyou for using number list application !!!");
+			{
+				System.out.println("enter string starting letter");
+				n=sc.next();
+				for(i=0;i<str.size();i++)
+				{
+
+				if (str.get(i).startsWith(n)){
+				System.out.println(str.get(i));
+				}
+				}
+				break;
+				}
+			case 6:
+				System.out.println("Thankyou for using strings list application !!!");
 				break;
 			default:
 				System.out.println("Please enter a valid number !!!");
 			}
 			
-			if(option==5)
+			if(option==6)
 			{		
 				break;
 			}
 		}
 
 	}
-
 }
-
-
